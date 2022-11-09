10890216965cf4a4be043108cc29561b051dbf76
这是几个名字 <aaabbbccc306@126.com>
2022-11-09 16:28:44 +0800

    pad_0

--- Questionaire2/app/src/main/java/com/farrirs/Questionaire2/Login.java
+++ Questionaire2/app/src/main/java/com/farrirs/Questionaire2/Login.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity{
@@ -24,25 +25,29 @@ public class Login extends AppCompatActivity{
     RadioButton rb_gender1;
     RadioButton rb_gender2;
     Button enter_button;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_pad);
         initView();
+
+        spinner = findViewById(R.id.spinner);
+
         enter_button = findViewById(R.id.enter_button);
         enter_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(rb_gender1.isChecked())gender="man";
                 else if(rb_gender2.isChecked())gender="woman";
-                name=input_name.getText().toString().trim();
-                age=input_age.getText().toString().trim();
+                name="默认姓名";
+                age=spinner.getSelectedItem().toString();;
 
                 if(gender.equals("")||name.equals("")||age.equals(""))
                 {
                     Toast.makeText(Login.this, "Please accomplish your input", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(Login.this, Login.class);
+                    Intent intent = new Intent(Login.this, Login_pad.class);
                     startActivity(intent);
                     return;
                 }
@@ -60,7 +65,5 @@ public class Login extends AppCompatActivity{
         rg_gender = findViewById(R.id.rg_gender);
         rb_gender1 = findViewById(R.id.rb_man);
         rb_gender2 = findViewById(R.id.rb_woman);
-        input_name = findViewById(R.id.input_name);
-        input_age = findViewById(R.id.input_age);
     }
 }
